f17ee5a421836871a0b70a89321c00d7a676feae
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-10-28 16:08:54 +0100

    Optimization

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,9 @@ export default function App() {
               src="/img/beach-work.jpg"
               alt="Woman workcationing on the beach"
             />
-            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+            <h1 className="mt-6 text-2xl font-headline tracking-tight font-semibold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
               You can work from anywhere.
-              <br className="hidden lg:inline" />{" "}
-              <span className="text-brand">Take advantage of it.</span>
+              <br className="hidden lg:inline" /> <span className="text-brand">Take advantage of it.</span>
             </h1>
             <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
               Workcation helps you find work-friendly rentals in beautiful
@@ -27,8 +26,9 @@ export default function App() {
             </p>
             <div className="mt-4 space-x-1 sm:mt-6">
               <a
-                className="inline-block px-5 py-3 rounded-lg transform transition bg-brand hover:bg-brand-light hover:-translate-y-0.5 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-brand-dark uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base"
-                href="#">
+                className="inline-block px-5 py-3 rounded-lg transform transition bg-brand hover:bg-brand-light hover:-translate-y-0.5 focus:ring-brand focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-brand-dark uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base"
+                href="#"
+              >
                 Book your escape
               </a>
             </div>
@@ -42,12 +42,10 @@ export default function App() {
           />
         </div>
       </div>
+
       <div className="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
         <h2 className="text-xl text-gray-900">Popular destinations</h2>
-        <p className="mt-2 text-gray-600">
-          A selection of great work-friendly cities with lots to see and
-          explore.
-        </p>
+        <p className="mt-2 text-gray-600">A selection of great work-friendly cities with lots to see and explore.</p>
         <div className="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
           {popularDestinations.map((destination) => (
             <DestinationCard destination={destination} key={destination.city} />
